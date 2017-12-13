ec54ec4ffa759355c2d7a665b3ee40e1d1761e31
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-12-13 16:30:51 -0500

    reorganized application into two modules

---
+++ image.py
@@ -0,0 +1,89 @@
+from app import app
+from collections import namedtuple
+from datetime import datetime
+from flask import json, redirect, url_for, request
+from itertools import groupby
+from operator import itemgetter, attrgetter
+from os import listdir
+from os.path import dirname, isfile, join
+from pprint import pprint as pp
+import re
+
+def get_image_listing_json(target_date, time=None):
+    images_path = app.config['APP_IMAGES_PATH']
+    mypath = join(app.config['APP_ROOT'], images_path)
+    files = listdir(mypath)
+
+    # get png files
+    png_files = [ f for f in files if isimage(f)]
+
+    imgs = [Image(parse_publication(f), parse_date(f), f) for f in png_files]
+    imgs = [i for i in imgs if matches_date(i.date, target_date)] # get only target date's images
+
+    pubs_dict = {}
+    # must sort dictionary before using itertools.groupby
+    imgs.sort(key=attrgetter('publication'))
+    for key, valuesiter in groupby(imgs, key=attrgetter('publication')):
+
+        # sort imgs by reverse chronological order
+        valuesiter = sorted(valuesiter, key=attrgetter('date'), reverse=True)
+
+        cropped = next((i for i in valuesiter if "cropped" in i.path), None)
+        original = next((i for i in valuesiter if i.date == cropped.date and i.path != cropped.path), None)
+
+        cropped_dict = { 'path' : join('/', images_path, cropped.path), \
+                        'date' : cropped.date }
+        original_dict = { 'path' : join('/', images_path, original.path), \
+                         'date' : original.date }
+
+        # get first cropped image
+
+        pubs_dict[key] = { 'cropped' : cropped_dict, \
+                          'original' : original_dict }
+
+    full_paths = [ join("/", images_path, png) for png in png_files ]
+
+    return json.jsonify(pubs=pubs_dict)
+
+Image = namedtuple('Image', 'publication date path')
+
+def parse_date(name):
+    """
+    :param name: filename as a string
+    :return: datetime object
+    """
+    # reference filename 2017-12-11-16_23_usatoday.png
+    date_format = '%Y-%m-%d-%H_%M_'
+    date_format_len = 17 # len("2017-12-11-16_23_")
+
+    date_part = name[0:date_format_len]
+
+    date = datetime.strptime(date_part, date_format)
+    return date
+
+def parse_publication(name):
+    """
+    Given '2017-12-11-16_23_usatoday.png', returns usatoday
+    :param name: filename as a string
+    :return: the publication key
+    """
+    date_format_len = len("2017-12-11-16_23")
+    date_format_len = 17 # len("2017-12-11-16_23_")
+
+    name = re.sub(r'[0-9_\-]*(.*)', r'\1', name)
+    publication = re.sub(r'(_cropped\.png|\.png)', r'', name)
+    return publication
+
+def isimage(f): return re.search('.*\.png$', f)
+
+def matches_date(date, target_date):
+    """
+    Return true if year, month, and day are the same
+    :param date: datetime object
+    """
+    return date.year == target_date.year and \
+           date.month == target_date.month and \
+           date.day == target_date.day
+
+
+
